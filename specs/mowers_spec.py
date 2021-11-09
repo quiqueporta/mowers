28c0e7fa39592c6743410bc662aa7da9474fd562
Quique Porta <quiqueporta@gmail.com>
2021-11-09 18:20:47 +0100

    Implement orientation

--- specs/mowers_spec.py
+++ specs/mowers_spec.py
@@ -1,10 +1,63 @@
 from mamba import description, context, it
-from expects import expect, be_true, be_false
+from expects import expect, be_true, be_false, equal
 
-from mowers import test
+from mowers import Mower
 
 
-with description("Mowers"):
+with description("Mower"):
 
-    with it("works"):
-        expect(test()).to(be_true)
+    with it("can rorate left once"):
+        mower = Mower()
+
+        result = mower.execute("L")
+
+        expect(result).to(equal("0 0 W"))
+
+    with it("can rorate left twice"):
+        mower = Mower()
+
+        result = mower.execute("LL")
+
+        expect(result).to(equal("0 0 S"))
+
+    with it("can rorate left three times"):
+        mower = Mower()
+
+        result = mower.execute("LLL")
+
+        expect(result).to(equal("0 0 E"))
+
+    with it("can rorate left four times"):
+        mower = Mower()
+
+        result = mower.execute("LLLL")
+
+        expect(result).to(equal("0 0 N"))
+
+    with it("can rotate right once"):
+        mower = Mower()
+
+        result = mower.execute("R")
+
+        expect(result).to(equal("0 0 E"))
+
+    with it("can rotate right twice"):
+        mower = Mower()
+
+        result = mower.execute("RR")
+
+        expect(result).to(equal("0 0 S"))
+
+    with it("can rotate right three times"):
+        mower = Mower()
+
+        result = mower.execute("RRR")
+
+        expect(result).to(equal("0 0 W"))
+
+    with it("can rotate right four times"):
+        mower = Mower()
+
+        result = mower.execute("RRRR")
+
+        expect(result).to(equal("0 0 N"))
--- src/mowers.py
+++ src/mowers.py
@@ -1,2 +1,62 @@
-def test():
-    return True
+class North:
+
+    def rotate_right(self):
+        return East()
+
+    def rotate_left(self):
+        return West()
+
+    def __str__(self):
+        return "N"
+
+
+class East:
+
+    def rotate_right(self):
+        return South()
+
+    def rotate_left(self):
+        return North()
+
+    def __str__(self):
+        return "E"
+
+
+class South:
+
+    def rotate_right(self):
+        return West()
+
+    def rotate_left(self):
+        return East()
+
+    def __str__(self):
+        return "S"
+
+
+class West:
+
+    def rotate_right(self):
+        return North()
+
+    def rotate_left(self):
+        return South()
+
+    def __str__(self):
+        return "W"
+
+
+class Mower:
+
+    def __init__(self):
+        self.__current_orientation = North()
+
+    def execute(self, commands: str):
+
+        for command in commands:
+            if command == "R":
+                self.__current_orientation = self.__current_orientation.rotate_right()
+            elif command == "L":
+                self.__current_orientation = self.__current_orientation.rotate_left()
+
+        return f"0 0 {self.__current_orientation}"
